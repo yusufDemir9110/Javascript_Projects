e568e18c37d9409fa9a6ffae48dced33a0873748
yusuf Demir <yusuf.ferrum@gmail.com>
2022-05-23 19:22:23 +0200

    FAQ done

---
+++ FAQ_Questions_Javascript/app.js
@@ -0,0 +1,29 @@
+//using selectors inside the element
+
+const questions = document.querySelectorAll(".question");
+
+questions.forEach((question) => {
+  const btn = question.querySelector(".question-btn");
+  btn.addEventListener("click", () => {
+    questions.forEach((item) => {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+    question.classList.toggle("show-text");
+    btn.classList.toggle("question-btn-rotate");
+  });
+});
+
+// traversing the dom
+
+// const btns = document.querySelectorAll(".question-btn");
+
+// btns.forEach((btn) => {
+//   btn.addEventListener("click", (e) => {
+//     const question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle("show-text");
+//     const btn = e.currentTarget;
+//     btn.classList.toggle("question-btn-rotate");
+//   });
+// });
